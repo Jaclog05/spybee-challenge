26cbc233081214e25004e694207115a7b82556f6
Jaider <jaidercueto.1010@gmail.com>
2026-06-17 20:16:13 -0500

    feat: Modificando estilos al Navbar. Creando logo en svg de Spybee para navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,12 +1,23 @@
 'use client'
-import Link from 'next/link'
+import Image from 'next/image'
+import styles from './Navbar.module.scss'
 
 export default function Navbar() {
-
   return (
-    <nav>
-      <Link href="/dashboard">Dashboard</Link>
-      <Link href="/crear-incidencia">Crear Incidencia</Link>
-    </nav>
+    <header className={styles.navbar}>
+      <Image
+        src="/spybee-logo.svg"
+        alt="Spybee"
+        width={100}
+        height={53}
+        priority
+      />
+      <div className={styles.project}>
+        <span>Proyecto Onboarding</span>
+      </div>
+      <div className={styles.avatar}>
+        <div className={styles.avatarCircle}>J</div>
+      </div>
+    </header>
   )
 }
